020e7c447ab5423b96e7cec919e10b7055b10ea7
Vince1220 <guitarvince025@yahoo.com>
2023-05-22 13:46:52 -0400

    Scheama Correction

--- app.py
+++ app.py
@@ -23,15 +23,76 @@ CORS(app)
 Migrate(app, db)
 
 # Models
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float)
+    inventory_quantity = db.Column(db.Integer)
 
-
+    def __repr__(self):
+        return f'{self.inventory_quantity} {self.price} {self.description} {self.name}'
 
 # Schemas
+class ProductSchema(ma.Schema):
+     id = fields.Integer(primary_key=True)
+    name = fields.String(required=True)
+    description = fields.String(required=True)
+    price = fields.Float()
+    inventory_quantity = fields.Integer()
+   
 
+    class Meta:
+        fields = ('id', 'name', 'description', 'price', 'inventory_quantity')
+
+    @post_load
+    def create_product(self, data, **kwargs):
+        return Product(**data)
+
+product_schema = ProductSchema()
+products_schema = ProductSchema(many=True)
 
 
 # Resources
+class ProductListResource(Resource):
+    def get(self):
+        all_products = Product.query.all()
+        return products_schema.dump(all_products)
 
+    def post(self):
+        form_data = request.get_json()
+        try:
+            new_product = product_schema.load(form_data)
+            db.session.add(new_product)
+            db.session.commit()
+            return product_schema.dump(new_product), 201
+        except ValidationError as err:
+            return err.messages, 400
+    
+class ProductResource(Resource):
+    def get(self, product_id):
+        product_from_db =Product.query.get_or_404(product_id)
+        return product_schema.dump(product_from_db)
+
+def delete(self, product_id):
+    Product_from_db = Product.query.get_or_404(product_id)
+    db.session.delete(Product_from_db)
+    return "", 204
+
+def put(self, product_id):
+    product_from_db = Product.query.get_or_404(product_id)
+    if 'name' in request.json:
+        product_from_db.name = request.json['name']
+    if 'description' in request.json:
+        product_from_db.description = request.json['description']
+    if 'price' in request.json:
+        product_from_db.price = request.json['price']
+    if 'inventory_quantity' in request.json:
+        product_from_db.inventory_quantity = request.json['inventory_quantity']
+    db.session.commit()
+    return product_schema.dump(product_from_db)
 
 
 # Routes
+api.add_resource(ProductListResource,"/api/products")
+api.add_resource(ProductResource, '/api/products/<int:product_id>')
